993f366d546d08483b55c3b9c7d3618ca7dd934b
Sohan <sriramsohan@gmail.com>
2023-06-15 22:34:33 +0530

    todo tested

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,6 +12,34 @@
 */
 
 class Todo {
+  constructor(){
+    this.todoList = [];
+  }
+  
+  add(todo) {
+    this.todoList.push(todo);
+  }
+  remove(index){
+    this.todoList.splice(index,1);
+  }
+  update(index,updatedTodo){
+    if(index < this.todoList.length){
+    this.todoList.splice(index,1,updatedTodo);
+  }
+}
+  getAll() {return this.todoList;  }
+
+  get(indexOfTodo){
+    if (indexOfTodo < this.todoList.length) {
+      return this.todoList[indexOfTodo];
+    } else {
+      return null;
+  }
+}
+
+  clear(){
+    this.todoList = [];
+  }
 
 }
 
